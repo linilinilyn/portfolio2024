a7bbeeea7d5c9fe736c930c936c1e01b85e561e1
sulyn <linilyn2023@gmail.com>
2024-01-15 13:17:56 +0900

    header완성

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-window.onload = function(){}
+window.onload = function(){
+    document.addEventListener("scroll", function(){
+        let scrollPosition = window.scrollY;
+        let sectionItem = document.querySelectorAll("section");
+        sectionItem.forEach(function(item){
+            let sectionTop = item.offsetTop - 200;
+            let sectionHeight = item.clientHeight;
+
+            if(scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight){
+                let sectionId = item.id
+                document.querySelector(".header ul li.active").classList.remove("active")
+                document.querySelector(".header ul li a[href ='#"+ sectionId +"']").parentNode.classList.add("active")
+            }
+        })
+    })
+}
 
 
 // window.onload = function(){}는 1번만 사용가능 
